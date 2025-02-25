57ba9ac50232f572450d60e5d66c6ee625a09d6d
elizat-nurdymbek <e_nurdymbek@kbtu.kz>
2025-02-25 13:04:33 +0500

    Пересоздал папку labs

--- 2 lab/function_1/4.py
+++ 2 lab/function_1/4.py
@@ -5,7 +5,7 @@
 def prime_san(a):
     if a < 2:
         return False
-    for i in range(2, int(a**0.5) + 1):
+    for i in range(2, a):
         if a % i == 0:
             return False
     return True
@@ -13,6 +13,6 @@ def prime_san(a):
 def filter_prime(numbers):
     return [num for num in numbers if prime_san(num)]
 
-numbers = list(map(int, input("Sandi engiz: ").split()))
+numbers = (map(int, input("Sandi engiz: ").split()))
 prime_numbers = filter_prime(numbers)
 print(f"Prime numbers: {prime_numbers}")
